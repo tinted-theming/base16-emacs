f41173ee630c97acd39ee81344a44adaf5970735
Kaleb Elwert <belak@coded.io>
2016-08-05 10:48:33 -0700

    Convert to new base16-builder format

--- base16-theme-autoloads.el
+++
@@ -1,7 +0,0 @@
-;;; base16-theme-autoloads.el --- setups the theme path
-;;
-;;; Code:
-
-(when (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;; base16-theme-autoloads.el ends here
--- base16-theme-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "base16" "1"
-  "Collection of themes built on combinations of 16 base colors"
-  nil
-  :url "https://github.com/mkaito/base16-emacs")
