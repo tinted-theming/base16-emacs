858c067dd05b1e1ef57e083504b094972ad3a318
Kaleb Elwert <belak@coded.io>
2017-03-17 11:47:06 -0700

    Update screenshot.sh to work properly

--- screenshots/screenshot.sh
+++ screenshots/screenshot.sh
@@ -2,11 +2,13 @@
 
 emacs -Q --load screenshot.el
 
-echo '* Base16 Theme Previews' > ./../gh-pages/index.org
+pushd ./../gh-pages/
 
-for file in *.png; do
-    echo -e "\n** ${file%.png}\n" >> ./../gh-pages/index.org
-    echo "[[./$file]]" >> ./../gh-pages/index.org
+echo '* Base16 Theme Previews' > index.org
+
+for file in samples/*.png; do
+    echo -e "\n** ${file%.png}\n" >> index.org
+    echo "[[./$file]]" >> index.org
 done
 
-emacs -Q ./../gh-pages/index.org --batch -f org-html-export-to-html --kill
+emacs -Q index.org --batch -f org-html-export-to-html --kill
