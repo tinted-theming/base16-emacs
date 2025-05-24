41c247a92c2a40028a31b5884c4bde43df1da137
Kaleb Elwert <kaleb@coded.io>
2025-05-23 23:54:31 -0700

    Update base16-theme.el

--- base16-theme.el
+++ base16-theme.el
@@ -274,13 +274,13 @@ return the actual color value.  Otherwise return the value unchanged."
 
 ;;; Third-party
 
-;;;; anzu-mode
-     (anzu-mode-line                               :foreground base0E)
-
 ;;;; adoc-mode
      (adoc-gen-face                                :foreground base0C)
      (adoc-verbatim-face                           :foreground base0A)
      (adoc-meta-face                               :foreground base03)
+
+;;;; anzu-mode
+     (anzu-mode-line                               :foreground base0E)
 
 ;;;; auctex
      (font-latex-bold-face                         :foreground base0B)
